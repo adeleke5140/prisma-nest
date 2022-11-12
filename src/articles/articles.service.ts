f001f2388ec969e6f9381e0513439d80df33a1a9
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-11-12 06:43:09 +0100

    update the article files

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,26 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
 export class ArticlesService {
+  constructor(private prisma: PrismaService) {}
   create(createArticleDto: CreateArticleDto) {
-    return 'This action adds a new article';
+    return this.prisma.article.create({ data: createArticleDto });
   }
 
   findAll() {
-    return `This action returns all articles`;
+    return this.prisma.article.findMany();
   }
 
+  findDrafts() {
+    return this.prisma.article.findMany({
+      where: {
+        published: false,
+      },
+    });
+  }
   findOne(id: number) {
-    return `This action returns a #${id} article`;
+    return this.prisma.article.findUnique({
+      where: {
+        id: id,
+      },
+    });
   }
 
   update(id: number, updateArticleDto: UpdateArticleDto) {
-    return `This action updates a #${id} article`;
+    return this.prisma.article.update({
+      where: { id },
+      data: updateArticleDto,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} article`;
+    return this.prisma.article.delete({
+      where: { id },
+    });
   }
 }
