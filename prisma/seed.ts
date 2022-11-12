5665878edb7b70b1bf92866491dc286fdfeb5d0e
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-11-12 06:42:51 +0100

    update prisma db files

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,36 +3,17 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const user1 = await prisma.author.create({
-    data: {
-      name: 'Kehinde',
-      email: 'kehinde@ken.com',
-      articles: {
-        create: {
-          title: 'Mongodb and Prisma launch',
-          body: 'support for mongodb has been one of the most requested features since the...',
-          description: 'we are excited to blah blah blah...',
-          published: false,
-        },
-      },
+  await prisma.article.update({
+    where: {
+      id: 1,
     },
-  });
-  const user2 = await prisma.author.create({
     data: {
-      name: 'phil',
-      email: 'phil@phil.com',
-      articles: {
-        create: {
-          title: 'Phil tries Prisma for the first time',
-          body: 'support for mongodb has been one of the most requested features since the...',
-          description: 'we are excited to blah blah blah...',
-          published: false,
-        },
-      },
+      published: true,
     },
   });
 
-  console.log(user1, user2);
+  const users = await prisma.article.findMany();
+  console.log(users);
 }
 
 main()
